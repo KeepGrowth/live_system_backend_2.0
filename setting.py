d5961361d4b35acf4ac78eb51c79f421b557d0e3
张同学 <zjun2002@outlook.com>
2026-04-30 11:03:29 +0800

    更改图片存储到夸克网盘中

--- setting.py
+++ setting.py
@@ -1,7 +1,8 @@
 import os
 
 # -------------------------图片上传设置------------------------------#
-UPLOAD_DIR = "uploads"
+# UPLOAD_DIR = "uploads"
+UPLOAD_DIR = "/vol02/1000-1-ad24ac70/夸克网盘/VueSystemFiles/live-system-2"
 # BASE_URL = "http://localhost:8888"  # 开发环境
 BASE_URL = "https://859707243.xyz:21355"  # 开发环境
 # 生产环境 uvicorn main:app --reload --port 8888
