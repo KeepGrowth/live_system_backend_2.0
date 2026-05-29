655e903f82ddfbe72a2ca4178610811318e4b821
张同学 <zjun2002@outlook.com>
2026-05-29 20:01:13 +0800

    引入异步Redis，但似乎用不上。

--- router/todo/todo.py
+++ router/todo/todo.py
@@ -70,7 +70,6 @@ async def get_todo_list(
         item.focus_time = sum(getattr(log, 'focus_time', 0) or 0 for log in item.todo_logs)
     todo_list = [TodoJoinItemResponse().model_validate(r) for r in result]
     res_data = TodoListResponse(todo_list=todo_list, total=total)
-    r.set
     return Result.success(data=res_data)
 
 
